be00ef80a3d94c9d5b776458a80c0ba51fa88147
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-29 10:14:39 +0300

    Edits on the padding

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -3,8 +3,8 @@ import hero from '../assets/hero.png'
 
 const Hero = () => {
   return (
-    <div id='hero' className='home w-full h-screen flex items-center justify-center lg:mb-6 bg-gradient-to-b from-gray-300 to-white lg:to-gray-100/50'>
-      <div className='w-full lg:h-full text-gray-800 flex flex-col lg:flex-row justify-center items-center px-8 md:px-16 lg:px-32 '>
+    <div id='hero' className='mb-12 home w-full h-full flex items-center justify-center lg:mb-6 bg-gradient-to-b from-gray-300 to-white lg:to-gray-100/50'>
+      <div className='mt-24 w-full h-full lg:h-screen lg:mt-0 text-gray-800 flex flex-col lg:flex-row justify-center items-center px-8 md:px-16 lg:px-32 '>
         <div className='w-full h-96 lg:h-full flex justify-center lg:mt-32'>
           <img src={hero} className='h-full object-cover' alt='hero'/>
         </div>
