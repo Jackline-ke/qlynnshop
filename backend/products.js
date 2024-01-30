94ab13a7d3e96220574abdc40ebe9240c20d82be
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-30 15:26:22 +0300

    styling the fetched data on the UI

--- backend/products.js
+++ backend/products.js
@@ -3,56 +3,56 @@ const products = [
         id: 1,
         name: "Cargo Pants",
         description: "Blue Cargo Pants",
-        price: "Ksh. 2000",
+        price: 2000,
         image: "https://res.cloudinary.com/dmpr1moai/image/upload/v1706532128/qlynnshop/___15_-removebg-preview_qm1tan.png"
     },
     {
         id: 2,
         name: "Casual Dress",
         description: "White Floral Dress",
-        price: "Ksh. 800",
+        price: 800,
         image: "https://res.cloudinary.com/dmpr1moai/image/upload/v1706532126/qlynnshop/Off_Shoulder_Shirred_Ditsy_Floral_Dress-removebg-preview_mmbwtc.png"
     },
     {
         id: 3,
         name: "Skirt",
         description: "Brown Cargo Skirt",
-        price: "Ksh. 1200",
+        price: 1200,
         image: "https://res.cloudinary.com/dmpr1moai/image/upload/v1706532125/qlynnshop/Flap_Pocket_Side_Cargo_Skirt-removebg-preview_lw4iuz.png"
     },
     {
         id: 4,
         name: "Jacket",
         description: "Black Leather Jacket",
-        price: "Ksh. 2500",
+        price: 2500,
         image: "https://res.cloudinary.com/dmpr1moai/image/upload/v1706532124/qlynnshop/6_Brands_That_Make_the_Perfect_Leather_Jacket_for_Under__500-removebg-preview_sglegf.png"
     },
     {
         id: 5,
         name: "Top",
         description: "Offshoulder Top",
-        price: "Ksh. 500",
+        price:  500,
         image: "https://res.cloudinary.com/dmpr1moai/image/upload/v1706532125/qlynnshop/RESTOCKS-removebg-preview_ylvyrb.png"
     },
     {
         id: 6,
         name: "Wide Leg",
         description: "Pink Ragged Wide Leg",
-        price: "Ksh. 1500",
+        price: 1500,
         image: "https://res.cloudinary.com/dmpr1moai/image/upload/v1706532124/qlynnshop/Solid_Flare_Leg_Jeans-removebg-preview_ojod8j.png"
     },
     {
         id: 7,
         name: "Pallazo",
         description: "Blue Pallazo",
-        price: "Ksh. 1800",
+        price: 1800,
         image: "https://res.cloudinary.com/dmpr1moai/image/upload/v1706532124/qlynnshop/Christopher_John_Rogers_-_Pleated_Wool_Wide-leg_Pants_-_Blue_-_US10-removebg-preview_oae8dc.png"
     },
     {
         id: 8,
         name: "Heels",
         description: "Black Chunk Heels",
-        price: "Ksh. 1500",
+        price: 1500,
         image: "https://res.cloudinary.com/dmpr1moai/image/upload/v1706532123/qlynnshop/___16_-removebg-preview_uhxqme.png"
     }
 ]
--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -5,22 +5,32 @@ import { useGetAllProductsQuery } from '../features/ProductsApi'
 const Products = () => {
     const {data, error, isLoading} = useGetAllProductsQuery()
   return (
-    <div>
+    <div className='w-full h-full px-8 md:px-16 lg:px-32 flex flex-col gap-4'>
         { isLoading ? (<p>Loading...</p>) : error ?
         (<p>An error occured...</p>) : ( 
         <>
-        <h1>Latest Women Wear</h1>
         <div>
-            <div>
-                {data?.map(product => <div key={product.is}>
-                    <img src={product.image} alt={product.name}/>
-                    <div>
-                        <h1>{product.name}</h1>
-                        <p>{product.description}</p>
-                        <p>{product.price}</p>
-                    </div>
-                    <button>Add To Cart</button>
-                </div>)}
+            <h1 className='text-xl lg:text-3xl'>Latest Women Wear</h1>
+        </div>
+        <div className='h-full'>
+            <div className=''>
+                <div className='grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-4 font-serif'>
+                    {data?.map(product => <div key={product.is}>
+                        <div className='flex flex-col items-center justify-center py-4 text-gray-800'>
+                           <div className='border border-gray-200/50 rounded-md w-full flex flex-col items-center py-4'>
+                                <img src={product.image} alt={product.name} className="h-32 object-cover"/>
+                           </div>
+                            <div className='text-center flex flex-col'>
+                                <p className='font-medium text-lg'>{product.name}</p>
+                                <p className='text-gray-800/70 text-sm'>{product.description}</p>
+                                <p className=''><span className='font-semibold'>Ksh. </span>{product.price}</p>
+                            </div>
+                           <div className='mt-1'>
+                                <button className='hover:bg-gray-800 hover:text-white rounded-md shadow-md text-sm px-4 border border-gray-800/30'>Add To Cart</button>
+                           </div>
+                        </div>
+                    </div>)}
+                </div>
             </div>
         </div>
         </>)}
